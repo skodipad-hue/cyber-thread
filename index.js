b5b259c3d3faa57aa9491042dabc242b70be9d02
saif-commits <149503864+saif-commits@users.noreply.github.com>
2025-12-29 23:37:43 +0530

    railway internal mysql connection

--- index.js
+++ index.js
@@ -30,7 +30,14 @@ const imagekit = new ImageKit({
 });
 
 /* ================= DATABASE (FINAL FIX) ================= */
-const db = mysql.createConnection(process.env.DATABASE_URL.trim());
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
+
 db.connect((err) => {
   if (err) {
     console.error("MySQL connection failed:", err);
